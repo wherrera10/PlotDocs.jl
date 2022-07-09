c670cedc80e949f13be0d6413c6411726194a332
Simon Christ <SimonChrist@gmx.de>
2022-07-09 20:41:05 +0000

    build based on 69d9847

--- versions.js
+++ versions.js
@@ -1,7 +1,13 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.30",
+  "v1.29",
+  "v1.28",
+  "v1.27",
+  "v1.26",
   "v1.25",
   "dev",
+  "latest",
 ];
-var DOCUMENTER_NEWEST = "v1.25.6";
+var DOCUMENTER_NEWEST = "v1.30.2";
 var DOCUMENTER_STABLE = "stable";
